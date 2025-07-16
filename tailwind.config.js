951b871ffb64102f53e161c6007039161ce8ff00
Deel <mac@PROs-MacBook-Pro.local>
2025-07-16 21:50:49 +0700

    🔧 Fix Tailwind config: include components + styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './styles/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Trebuchet MS', 'sans-serif']
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
