ebd1f9e3bf7545dce3c15e47f3933cdb91734f49
Deel <mac@PROs-MacBook-Pro.local>
2025-07-26 01:51:41 +0700

    clean: remove unused BACKUP folder

--- BACKUP/selfify-20250725-101243/components/BizBoostCard.tsx
+++
@@ -1,7 +0,0 @@
-export default function BizBoostCard() {
-  return (
-    <div className="bg-[#23272F] rounded-2xl px-4 py-3 flex items-center min-h-[46px]">
-      <span className="font-bold text-white text-lg">BizBoost</span>
-    </div>
-  );
-}
